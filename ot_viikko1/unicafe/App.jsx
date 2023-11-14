cc4356f4aa580fd1e0adc92e1dab3f425446a773
Nelli Sviili <nelli.sviili@gmail.com>
2023-11-14 13:14:53 +0100

    teht 1.8 unicafe step3 valmis

--- ot_viikko1/unicafe/App.jsx
+++ ot_viikko1/unicafe/App.jsx
@@ -1,8 +1,9 @@
-// tehtävä 1.7 unicafe step2
+// tehtävä 1.8 unicafe step2
 // Unicafe-ruokalan asiakaspalauteappi
 // kerää asiakastyytyväisyydestä: good/neutral/bad 
 // kertoo kyselyn tuloksen
 // lisätty arvot total, avarage, positive
+// siirretty statistiikka omaksi funktiokseen
 
 import { useState } from 'react'
 
@@ -11,6 +12,23 @@ const Button = ({ handleClick, text }) => (
     {text}
   </button>
 )
+const Statistics = ({ good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const avarage = (good - bad) / total
+  const positive = good / total * 100
+
+  return (
+    <div>
+    <h2>statistics</h2>
+      good {good}<br />
+      neutral {neutral}<br />
+      bad {bad}<br />
+      total {total}<br />
+      avarage {avarage}<br />
+      positive {positive} %<br />
+    </div>
+  )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -33,24 +51,14 @@ const App = () => {
     setBad(bad + 1)  
   }
 
-  const total = good + neutral + bad
-  const avarage = (good - bad) / total
-  const positive = good / total * 100
-
   return (
     <div>
       <h1>UNICAFE</h1>
-      <h1>Give Your Feedback</h1>
+      <h2>Give Your Feedback</h2>
       <Button handleClick={giveGood} text="good" />
       <Button handleClick={giveNeutral} text="neutral" />
       <Button handleClick={giveBad} text="bad" />
-      <h1>statistics</h1>
-      good {good}<br />
-      neutral {neutral}<br />
-      bad {bad}<br />
-      total {total}<br />
-      avarage {avarage}<br />
-      positive {positive} %<br />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
